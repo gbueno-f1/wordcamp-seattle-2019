c938976bee7f1232f6bcaac1b61dd1c751c44f84
Greg Bueno <gbueno@forumone.com>
2019-10-13 14:34:59 -0700

    Adding MVC code example.

---
+++ services/wordpress/public/wp-content/plugins/wordcamp-seattle-2019/lib/Models/Events.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace ForumOne\WordPress\Plugins\WordCamp\SamplePlugin\Models;
+
+
+class Events
+{
+    /**
+     * Query Salsa Engage API for events.
+     *
+     * query() connects to the Salsa Engage API and returns a list of events.
+     *
+     * @param array $options
+     * @return array|mixed|object
+     *
+     * @since 1.0.1
+     *
+     * @uses wp_remote_get()
+     * @uses wp_remote_retrieve_body()
+     */
+    public static function query($options = array() ) {
+
+    }
+}
